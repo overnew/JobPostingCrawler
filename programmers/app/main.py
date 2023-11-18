c8042b933f1bb136fec6efdee6ca27772ab2f261
overnew <ehdls1303@gmail.com>
2023-11-18 12:58:07 +0900

    trigger actions

--- programmers/app/main.py
+++ programmers/app/main.py
@@ -5,5 +5,5 @@ from SlackNotifiaction import SnsWrapper
 try:
     ProgrammersCrawler().crawling_start()
     uploadCsvToCloud()
-except:
+except: #에러 시에만 알림
     SnsWrapper.publish_crawling_message("프로그래머스 크롤링 에러!")
